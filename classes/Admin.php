addbd5ca3dcd1a23b78cc93e73e314069343dcf1
ErionMeh <erionm462@gmail.com>
2025-04-16 16:30:21 +0200

    Update Admin.php

--- classes/Admin.php
+++ classes/Admin.php
@@ -1,15 +1,39 @@
 <?php
 
+require_once 'User.php';
 
+class Admin extends User {
 
-class Admin extends User{
+    private $accessLevel;
 
+    public function __construct($id = 0, $fullname = '', $email = '', $password = '', $accessLevel = 'full') {
+        parent::__construct($id, $fullname, $email, $password);
+        
+        $this->role = 'admin';
+        $this->accessLevel = $accessLevel;
+    }
 
-    
+    public function getAccessLevel() {
+        return $this->accessLevel;
+    }
+
+    public function setAccessLevel($accessLevel) {
+        $this->accessLevel = $accessLevel;
+    }
+
+    public function login($email, $password) {
+        if ($email === $this->getEmail() && $password === $this->getPassword()) {
+            return true;
+        }
+        return false;
+    }
+
+    public function canManageProducts() {
+        return ($this->accessLevel === 'full' || $this->accessLevel === 'product');
+    }
+
+    public function canManageUsers() {
+        return ($this->accessLevel === 'full' || $this->accessLevel === 'user');
+    }
 }
-
-
-
-
-
 ?>
