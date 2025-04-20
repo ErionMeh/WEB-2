abb14aa1a0d5ec72a63441040f98ae0583dc787e
albanimani <alba.nimani@student.uni-pr.edu>
2025-04-20 17:00:31 +0200

    Update i register, manipulimi me numra  dhe perdorimi i regex per rregullimin e permbajtjes se stingjeve

--- register.php
+++ register.php
@@ -1,7 +1,51 @@
 <?php include('includes/header.php'); ?>
 
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fullname = $_POST['fullname'];
+    $email = $_POST['email'];
+    $password1 = $_POST['password1'];
+    $password2 = $_POST['password2'];
+    $phone = $_POST['phone'] ?? '';
 
-<!-- Login -->
+
+    $errors = [];
+
+    if (!preg_match("/^[a-zA-Z\s]+$/", $fullname)) {
+        $errors[] = "Emri duhet të përmbajë vetëm shkronja dhe hapësira.";
+    }
+
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Email-i nuk është valid.";
+    }
+
+    if (!preg_match("/^\d{3}-\d{3}-\d{3}$/", $phone)) {
+        $errors[] = "Numri i telefonit duhet të jetë në formatin 044-123-456.";
+    }
+    
+
+    
+    if (!preg_match("/^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/", $password1)) {
+        $errors[] = "Fjalëkalimi duhet të ketë së paku 8 karaktere, një shkronjë të madhe, një numër dhe një simbol.";
+    }
+    
+
+    
+    if ($password1 !== $password2) {
+        $errors[] = "Fjalëkalimet nuk përputhen.";
+    }
+
+    if (empty($errors)) {
+        echo "<div class='alert alert-success'>Regjistrimi u bë me sukses!</div>";
+    } else {
+        foreach ($errors as $error) {
+            echo "<div class='alert alert-danger'>$error</div>";
+        }
+    }
+}
+?>
+
+
 <div class="auth py-5">
     <div class="container">
         <div class="row">
@@ -11,7 +55,8 @@
             <div class="col-lg-5 offset-lg-1 col-md-5 offset-md-1 col-sm-12 offset-sm-0 d-flex align-items-center">
                 <div class="login w-100">
                     <h2>Register</h2>
-                    <form action="#">
+                    <form method="POST" action="">
+
                         <div class="form-group">
                             <label for="fullname my-2">Fullname</label>
                             <input type="text" name="fullname" id="fullname" class="form-control" placeholder="Enter your name and surname" />
@@ -20,6 +65,11 @@
                             <label for="email">Email</label>
                             <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email" />
                         </div>
+                        <div class="form-group my-4">
+                           <label for="phone">Phone Number</label>
+                             <input type="text" name="phone" id="phone" class="form-control" placeholder="e.g. 044-123-456" />
+                         </div>
+
                         <div class="form-group my-4">
                             <label for="password">Password</label>
                             <input type="password" name="password1" id="password" class="form-control" placeholder="Enter your password" />
