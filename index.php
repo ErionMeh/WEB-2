bc5e7a0ef8a27f77d67093e062abab41aa88e882
alba.nimani <alba.nimani@student.uni-pr.edu>
2025-05-16 13:57:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include('includes/header.php'); ?>
 
-<!-- Promotions -->
+
 <div class="promotions">
     <div id="carouselExampleCaptions" class="carousel slide">
         <div class="carousel-indicators">
@@ -10,26 +10,35 @@
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
-            <img src="https://hhstsyoejx.gjirafa.net/gj50/banners/mb-d-1677060548.jpg" class="d-block w-100" alt="...">
+            <img src="assets/img/6988236.jpg" class="d-block w-100" alt="First slide">
             <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>Some representative placeholder content for the first slide.</p>
+               
             </div>
             </div>
-            <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>Some representative placeholder content for the second slide.</p>
-            </div>
-            </div>
-            <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>Some representative placeholder content for the third slide.</p>
-            </div>
-            </div>
+            
+<div class="carousel-item">
+    <div class="nude-slide">
+        <div class="image-wrapper">
+            <img src="assets/img/3.jpg" class="centered-img" alt="Second slide">
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+        
+        </div>
+    </div>
+</div>
+
+
+<div class="carousel-item">
+    <div class="nude-slide">
+        <div class="image-wrapper">
+            <img src="assets/img/2.webp" class="centered-img" alt="Third slide">
+        </div>
+        <div class="carousel-caption d-none d-md-block">
+            
+        </div>
+    </div>
+</div>
+
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -42,12 +51,12 @@
     </div>
 </div>
 
-<!-- Latest products -->
+
 <div class="latest-products bg-light py-5">
     <div class="container">
         <div class="text-center mb-5">
             <h2>Latest products</h2>
-            <p>45.960 products available</p>
+           
         </div>
         <div class="row">
             <div class="col-lg-3 col-md-3 col-sm-12">
@@ -55,22 +64,18 @@
                     <img src="assets/img/Iphone.png" class="card-img-top" alt="iPhone 14 Pro">
                     <div class="card-body">
                         <h5 class="card-title">iPhone 14 Pro</h5>
-                        <p class="card-text">2450.50 &euro;</p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                       
+                       
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
+                </div> 
+            </div> 
             <div class="col-lg-3 col-md-3 col-sm-12">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/img/Samsung.png" class="card-img-top" alt="Samsung Galaxy S23 Ultra">
                     <div class="card-body">
                         <h5 class="card-title">Samsung Galaxy S23 Ultra</h5>
-                        <p class="card-text">
-                            <strike>2450.50 &euro;</strike>
-                            <span class="badge bg-danger">10%</span>
-                            <span>2120 &euro;</span>
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                       
+                       
                     </div>
                 </div> <!-- ./card -->
             </div> <!-- ./col -->
@@ -79,81 +84,68 @@
                     <img src="assets/img/Apple Watch.png" class="card-img-top" alt="Apple Watch Series 8">
                     <div class="card-body">
                         <h5 class="card-title">Apple Watch Series 8</h5>
-                        <p class="card-text">899.99 &euro;</p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                       
+                       
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
+                </div> 
+            </div> 
             <div class="col-lg-3 col-md-3 col-sm-12">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/img/Airpods.png" class="card-img-top" alt="AirPods Pro 2">
                     <div class="card-body">
                         <h5 class="card-title">AirPods Pro 2</h5>
-                        <p class="card-text">
-                            <strike>199.99 &euro;</strike>
-                            <span class="badge bg-danger">15%</span>
-                            <span>169.99 &euro;</span>
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                       
+                     
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
+                </div> 
+            </div> 
             <div class="col-lg-3 col-md-3 col-sm-12 mt-4">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/img/Asus.png" class="card-img-top" alt="ASUS ROG Strix G15">
                     <div class="card-body">
                         <h5 class="card-title">ASUS ROG Strix G15</h5>
-                        <p class="card-text">1899.99 &euro;</p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                        
+                    
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
+                </div>
+            </div> 
             <div class="col-lg-3 col-md-3 col-sm-12 mt-4">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/img/LG.png" class="card-img-top" alt="LG OLED C2 65-inch">
                     <div class="card-body">
                         <h5 class="card-title">LG OLED C2 65-inch</h5>
-                        <p class="card-text">
-                            <strike>1299.99 &euro;</strike>
-                            <span class="badge bg-danger">20%</span>
-                            <span>1039.99 &euro;</span>
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                      
+                  
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
+                </div> 
+            </div> 
             <div class="col-lg-3 col-md-3 col-sm-12 mt-4">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/img/JBL.png" class="card-img-top" alt="JBL Charge 5">
                     <div class="card-body">
                         <h5 class="card-title">JBL Charge 5</h5>
-                        <p class="card-text">129.99 &euro;</p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                       
+                     
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
+                </div> 
+            </div>
             <div class="col-lg-3 col-md-3 col-sm-12 mt-4">
                 <div class="card" style="width: 18rem;">
                     <img src="assets/img/sony.png" class="card-img-top" alt="Sony Alpha a7 III">
                     <div class="card-body">
                         <h5 class="card-title">Sony Alpha a7 III</h5>
-                        <p class="card-text">
-                            <strike>749.99 &euro;</strike>
-                            <span class="badge bg-danger">25%</span>
-                            <span>562.49 &euro;</span>
-                        </p>
-                        <a href="#" class="btn btn-outline-secondary">View product</a>
+                     
                     </div>
-                </div> <!-- ./card -->
-            </div> <!-- ./col -->
-        </div> <!-- ./row -->
+                </div> 
+            </div> 
+        </div> 
         <div class="text-center mt-5">
-            <a href="#" class="btn btn-sm btn-outline-primary">Shop page &rarr;</a>
+            <a href="shop.php" class="btn btn-sm btn-outline-primary">Shop page &rarr;</a>
         </div>
     </div>
 </div>
 
-<!-- About eStore -->
+
 <div class="about-us py-5">
     <div class="container">
         <div class="row">
@@ -163,10 +155,10 @@
             <div class="col-lg-6 offset-lg-1 col-md-6 offset-md-1 col-sm-12 offset-sm-0">
                 <h2>eStore</h2>
                 <p>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat veritatis perferendis a accusantium nemo eum voluptatibus maxime optio, non blanditiis? Ipsa ab error aliquid dolorum, quasi dolor illo quam corrupti!
+                 eStore është destinacioni juaj online për blerje të shpejta, të sigurta dhe moderne. Me një gamë të gjerë produktesh – nga teknologjia, moda e deri te aksesorët e përditshëm – ju mund të gjeni gjithçka që ju nevojitet në një vend të vetëm.
                 </p>
                 <p class="mt-2">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat earum error molestiae exercitationem velit at corporis porro cumque minus ducimus, quia ex quo consectetur veniam tenetur reiciendis ratione quisquam qui.
+                     Ne ofrojmë çmime konkurruese, transport të shpejtë dhe shërbim të shkëlqyer ndaj klientit. Regjistrohuni sot dhe përjetoni blerjen më të lehtë që keni provuar ndonjëherë!
                 </p>
             </div>
         </div>
